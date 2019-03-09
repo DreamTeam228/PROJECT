eebef495e02a670f460a57d1afb77b7e798210e3
DreamTeam228 <48161799+DreamTeam228@users.noreply.github.com>
2019-03-09 11:07:32 +0300

    Add files via upload

--- pyqt/settings.py
+++ pyqt/settings.py
@@ -155,6 +155,7 @@ class SettingsScrapper(QWidget):
         self.PhoneNumbers = ''
         self.Price = ''
         self.Place = ''
+        self.Metro = ''
         nameBlockPhone = []
         numberBlockPhone = []
         nameBlockPrice = []
