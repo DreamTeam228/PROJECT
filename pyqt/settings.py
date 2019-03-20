3a6f7fc55070de66e37d929c80998c2eafa3c4f1
DreamTeam228 <48161799+DreamTeam228@users.noreply.github.com>
2019-03-20 14:50:30 +0300

    Add files via upload

--- pyqt/settings.py
+++ pyqt/settings.py
@@ -11,18 +11,19 @@ from PyQt5.QtWidgets import (QWidget,
                                                                QTextEdit, 
                                                                QLabel)
 import sys
+import os
 from scrap import (Scrap, 
                                       convertUrl)
 
 class SettingsScrapper(QWidget):
     soup = ""
-    PhoneNumbers = "PhoneNumbers: "
+    PhoneNumbers = ""
    # PhoneNumbers = ""
-    Price = "Price: "
+    Price = ""
     #Price = ""
-    Place = "Adress: "
+    Place = ""
     #Place = ""
-    Metro = "Metro: "
+    Metro = ""
     #Metro = ""
 
     def __init__(self):
@@ -89,81 +90,124 @@ class SettingsScrapper(QWidget):
         
         
     def RunScrap(self):
+        self.text.setText('')
+        self.Clear()
         
         Url = self.url.text()
+        self.soup = convertUrl(Url)
         
         print(Url)
-        nameBlockPhone = (self.nameblockphone.text()).split(",")
-        print(nameBlockPhone)
-        numberBlockPhone = (self.numberblockphone.text()).split(",")
-        NumberBlockPhone = []
-        for i in range(0, len(numberBlockPhone)):
-            NumberBlockPhone.append(int(numberBlockPhone[i]))
-
-        self.soup = convertUrl(Url)
+        if self.nameblockphone.text() == '':
+            print('Name and Number block phone was skiped')
+        else:
+            nameBlockPhone = (self.nameblockphone.text()).split(",")
+            print(nameBlockPhone)
+            numberBlockPhone = (self.numberblockphone.text()).split(",")
+            NumberBlockPhone = []
+            for i in range(0, len(numberBlockPhone)):
+                NumberBlockPhone.append(int(numberBlockPhone[i]))
 #==============================ДЛЯ НОМЕРОВ ТЕЛЕФОНОВ=============================
-        for i in range(0, len(nameBlockPhone)):
-            for n in range(0, len(numberBlockPhone)):
-                a = ((Scrap(self.soup, nameBlockPhone[i], int(NumberBlockPhone[n]))).replace(" ", "")).replace("\xa0", "")
-                self.PhoneNumbers = self.PhoneNumbers + a + " "
-        self.textEdit(self.PhoneNumbers)
+            for i in range(0, len(nameBlockPhone)):
+                for n in range(0, len(numberBlockPhone)):
+                    a = ((Scrap(self.soup, nameBlockPhone[i], int(NumberBlockPhone[n]))).replace(" ", "")).replace("\xa0", "")
+                    self.PhoneNumbers = self.PhoneNumbers + a + " "
+            self.textEdit(self.PhoneNumbers)
 #=======================РАБОТА НАД НОМЕРАМИ ТУТ ЗАКАНЧИВАЕТСЯ=====================
-        nameBlockPrice = (self.nameblockprice.text()).split(",")
-        numberBlockPrice = (self.numberblockprice.text()).split(",")
-        NumberBlockPrice = []
-        for i in range(0, len(numberBlockPrice)):
-            NumberBlockPrice.append(int(numberBlockPrice[i]))
+        if self.nameblockprice.text() == '':
+            print('Name and Number block price was skiped')
+        else:
+            nameBlockPrice = (self.nameblockprice.text()).split(",")
+            numberBlockPrice = (self.numberblockprice.text()).split(",")
+            NumberBlockPrice = []
+            for i in range(0, len(numberBlockPrice)):
+                NumberBlockPrice.append(int(numberBlockPrice[i]))
 #====================================ДЛЯ ЦЕННИКА===================================
-        for i in range(0, len(nameBlockPrice)):
-            for n in range(0, len(numberBlockPrice)):
-                a = ((Scrap(self.soup, nameBlockPrice[i], int(NumberBlockPrice[n]))).replace(" ", "")).replace("\xa0", "").replace("рублей", "")
-                self.Price = self.Price + a + " "
-        self.textEdit(self.Price)
+            for i in range(0, len(nameBlockPrice)):
+                for n in range(0, len(numberBlockPrice)):
+                    a = ((Scrap(self.soup, nameBlockPrice[i], int(NumberBlockPrice[n]))).replace(" ", "")).replace("\xa0", "").replace("рублей", "")
+                    self.Price = self.Price + a + " "
+            self.textEdit(self.Price)
 #=======================РАБОТА НАД ЦЕННИКОМ ТУТ ЗАКАНЧИВАЕТСЯ=====================
-        NameBlockPlace = []
-        NameBlockPlace.append(self.nameblockplace.text())
-        numberBlockPlace = (self.numberblockplace.text()).split(",")
-        NumberBlockPlace = []
-        for i in range(0,len(numberBlockPlace)):
-            NumberBlockPlace.append(int(numberBlockPlace[i]))
-#====================================ДЛЯ АДРЕСА===================================
-        for i in range(0, len(NameBlockPlace)):
-            for n in range(0, len(NumberBlockPlace)):
-                a =  Scrap(self.soup, NameBlockPlace[i], int(NumberBlockPlace[n]))
-                self.Place = self.Place + a + " "
-        self.textEdit(self.Place)
+        if self.nameblockplace.text() == '':
+            print('Name and Number block place was skiped')
+        else:
+            NameBlockPlace = []
+            NameBlockPlace.append(self.nameblockplace.text())
+            numberBlockPlace = (self.numberblockplace.text()).split(",")
+            NumberBlockPlace = []
+            for i in range(0,len(numberBlockPlace)):
+                NumberBlockPlace.append(int(numberBlockPlace[i]))
+    #====================================ДЛЯ АДРЕСА===================================
+            for i in range(0, len(NameBlockPlace)):
+                for n in range(0, len(NumberBlockPlace)):
+                    a =  Scrap(self.soup, NameBlockPlace[i], int(NumberBlockPlace[n]))
+                    self.Place = self.Place + a + " "
+            self.textEdit(self.Place)
 #============================РАБОТА НАД АДРЕСОМ ЗАКОНЧЕНА========================
-        NameBlockMetro = (self.nameblockmetro.text()).split(',')
-        numberBlockMetro = (self.numberblockmetro.text()).split(',')
-        NumberBlockMetro = []
-        for i in range(0, len(numberBlockMetro)):
-            NumberBlockMetro.append(int(numberBlockMetro[i]))
-#====================================ДЛЯ МЕТРО====================================
-        for i in range(0, len(NameBlockMetro)):
-            for n in range(0, len(NumberBlockMetro)):
-                a = Scrap(self.soup, NameBlockMetro[i], NumberBlockMetro[n])
-                self.Metro = self.Metro + a + " "
-        self.textEdit(self.Metro)
+        if self.nameblockmetro.text() == '':
+            print('Name and Number block metro was skiped')
+        else:
+            NameBlockMetro = (self.nameblockmetro.text()).split(',')
+            numberBlockMetro = (self.numberblockmetro.text()).split(',')
+            NumberBlockMetro = []
+            for i in range(0, len(numberBlockMetro)):
+                NumberBlockMetro.append(int(numberBlockMetro[i]))
+    #====================================ДЛЯ МЕТРО====================================
+            for i in range(0, len(NameBlockMetro)):
+                for n in range(0, len(NumberBlockMetro)):
+                    a = Scrap(self.soup, NameBlockMetro[i], NumberBlockMetro[n])
+                    self.Metro = self.Metro + a + " "
+            self.textEdit(self.Metro)
+            print(self.Metro)
 #=============================РАБОТА НАД МЕТРО ЗАКОНЧЕНА=========================
-        self.Clear()
-
+#=============================СОХРАНЕНИЕ ЕЩЕ НЕ РАБОТАЕТ==========================
     def Save(self):
-        NewFile = open()
+        path = os.getcwd() + '/'
+        Path = 'sites'
+        path = path + Path + '/'
+        current_place = path + self.url.text().replace('https://', '').replace('http://', '').replace('www.', '')
+        current_place = current_place + '/'
+        try:  
+            os.makedirs(current_place, 0o777)
+        except FileExistsError:  
+            print('Path was already created')
+        else:  
+            print('Path created')
+        file_price = 'Price'
+        file_phonenumbers = 'PhoneNumbers'
+        file_place = 'Place'
+        file_metro = 'Metro'
 
+        if self.nameblockprice.text() != '':
+            self.File(current_place, file_price, self.Price)
+        
+        if self.nameblockplace.text() != '':
+            self.File(current_place, file_place, self.Place)
+        
+        if self.nameblockmetro.text() != '':
+            self.File(current_place, file_metro, self.Metro)
+        
+        if self.nameblockphone.text() != '':
+            self.File(current_place, file_phonenumbers, self.PhoneNumbers)
+        #price = open(current_place + file_price + '.txt',"w+")
+        #price.close()
+        #price = open(current_place + file_price + '.txt',"w")
+        #price.write(str(self.Price))
+        #price.close()
+    
+    def File(self, Path, Name, text):
+        File = open(Path + Name + '.txt', 'w+')
+        File.close()
+        File = open(Path + Name + '.txt', 'w')
+        File.write(str(text))
+        print(str(text))
+        File.close()
 
     def Clear(self):
         self.PhoneNumbers = ''
         self.Price = ''
         self.Place = ''
         self.Metro = ''
-        nameBlockPhone = []
-        numberBlockPhone = []
-        nameBlockPrice = []
-        numberBlockPrice = []
-        NameBlockPlace = []
-        numberBlockPlace = []
-        NameBlockMetro = []
-        NumberBlockMetro = []
 
     def textEdit(self, string):
         self.text.append(string)
